29e3c6edf3f9ec7e1a9120141c4677b3754dc051
Public Profile <latchkey@gmail.com>
2026-07-20 19:50:00 -0700

    refactor: centralize shared icon paths

---
+++ src/lib/icon-paths.ts
@@ -0,0 +1,20 @@
+export const ICON_PATHS: Record<string, string> = {
+	bike: 'M5 17a4 4 0 1 1 0-8 4 4 0 0 1 0 8Zm14 0a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM9 13h6l-3-7-2 4h4M15 13l2-4',
+	bluetooth: 'm7 7 10 10-5 5V2l5 5L7 17',
+	bolt: 'm13 2-8 11h6l-1 9 8-12h-6l1-7Z',
+	cadence: 'M20 11a8 8 0 0 0-15.5-2M4 4v5h5M4 13a8 8 0 0 0 15.5 2M20 20v-5h-5',
+	controls: 'M7 4v6m0 4v6m10-16v2m0 4v10M4 10h6m4-4h6M4 14h6m4-4h6',
+	heart: 'M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.7l-1-1.1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 21l7.8-7.6a5.5 5.5 0 0 0 1-8.8Z',
+	imported:
+		'M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8m-6-6 6 6m-6-6v6h6M8 15l2 2 5-5',
+	minus: 'M5 12h14',
+	'move-vertical': 'M8 7l4-4 4 4M12 3v18M8 17l4 4 4-4',
+	pause: 'M9 5v14m6-14v14',
+	play: 'm9 5 10 7-10 7V5Z',
+	plus: 'M12 5v14M5 12h14',
+	resistance: 'M4 7h10m4 0h2M14 4v6M4 17h2m4 0h10M8 14v6',
+	route: 'M4 17c3-7 5-9 9-9 3 0 4 2 7 2M4 17h4m-4 0v-4M20 7h-4m4 0v4',
+	speed: 'M4 16a8 8 0 1 1 16 0M12 16l4-5M7 20h10',
+	stop: 'M6 6h12v12H6z',
+	upload: 'M12 16V4m0 0 4 4m-4-4L8 8M5 20h14',
+};
